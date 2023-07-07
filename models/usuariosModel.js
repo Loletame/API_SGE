2798ed3bd9ab2caaba1103d68eeca63d155dcdac
Loletame <loletame0100@gmail.com>
2023-07-07 19:53:44 -0300

    ...

--- models/usuariosModel.js
+++ models/usuariosModel.js
@@ -1,8 +1,6 @@
 const db = require('./../config/db');
 const { Router } = require("express");
 
-// tiene que ser asincronica porque todo se ejecuta al mismo tiempo y de esa manera anda
-// el console log
 
 exports.obtenerUsuarios = async () => {
     const [rows, fiels] = await db.execute('SELECT * FROM usuarios')
